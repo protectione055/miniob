57b49f0b4c12ce9856ffb4b75ecd32283ebde69b
TJRyan <tjr968368@gmail.com>
2022-10-18 07:55:20 +0000

    drop table

--- src/observer/storage/index/bplus_tree_index.cpp
+++ src/observer/storage/index/bplus_tree_index.cpp
@@ -48,6 +48,12 @@ RC BplusTreeIndex::create(const char *file_name, const IndexMeta &index_meta, co
   return RC::SUCCESS;
 }
 
+RC BplusTreeIndex::drop()
+{
+  index_handler_.drop();
+  return RC::SUCCESS;
+}
+
 RC BplusTreeIndex::open(const char *file_name, const IndexMeta &index_meta, const FieldMeta &field_meta)
 {
   if (inited_) {
